e404c9a37d04bbd9c810235d200a9967bd87e884
Ignacio Presas <cu.ouza@gmail.com>
2023-02-18 12:29:36 -0300

    setup

---
+++ some-cool-name-for-my-courses-repo/React and TypeScript v2/src/components/ControlPanel.tsx
@@ -0,0 +1,27 @@
+type ControlPanelProps = {
+  name: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  greeting: string;
+};
+
+const ControlPanel = ({ name, greeting, onChange }: ControlPanelProps) => {
+  return (
+    <form
+      className="flex flex-row gap-4 text-white bg-primary-10"
+      onSubmit={(event) => event.preventDefault()}
+    >
+      <div>
+        <label className="font-bold">Your Name</label>
+        <input
+          name="name"
+          className="w-full"
+          type="text"
+          value={name}
+          onChange={onChange}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default ControlPanel;
