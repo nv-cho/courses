e4857df5ad5076a2724f8cc814a5670c8c839d81
Ignacio Presas <cu.ouza@gmail.com>
2023-02-27 15:18:54 -0300

    .

--- some-cool-name-for-my-courses-repo/color-theory/README.md
+++
@@ -1,3 +0,0 @@
-# React and TypeScript Example
-
-This is an exmaple application from Steve's React and TypeScript course.
--- some-cool-name-for-my-courses-repo/color-theory/src/components/hex-color.tsx
+++
@@ -1,9 +0,0 @@
-type HexColorProps = {
-  hexColor: string;
-};
-
-const HexColor = ({ hexColor }: HexColorProps) => {
-  return <section className="font-semibold information">{hexColor}</section>;
-};
-
-export default HexColor;
